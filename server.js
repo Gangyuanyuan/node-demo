4bb144ecabe7e630e9dca06c79d176242901d57e
Gangyuan <296671396@qq.com>
2019-05-18 13:00:46 +0800

    SRJ

--- server.js
+++ server.js
@@ -1,75 +1,57 @@
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
-var port = process.argv[2]
-
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
-}
+var port = process.env.PORT || 8888;
 
 var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log('HTTP 路径为\n' + path)
-  console.log('查询字符串为\n' + query)
-  console.log('不含查询字符串的路径为\n' + pathNoQuery)
   
-  if(path == '/style.css'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: blue;}')
-    response.end()
-  }else if(path == '/main.js'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("现在是由JS执行的")')
-    response.end()
-  }else if(path == '/'){
+
+  if(path === '/'){ // 如果用户请求的是 / 路径
+    var string = fs.readFileSync('./index.html', 'utf8') // 读取
+    var amount = fs.readFileSync('./db', 'utf8') // 100
+    string = string.replace('&&&amount&&&', amount) // 替换内容
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.css">' +
-      '</head><body>'  +
-      '<h1>你好，饥人谷</h1>' +
-      '<script src="/main.js"></script>' +
-      '</body></html>')
+    response.write(string)
     response.end()
+  }else if(path === '/style.css'){
+    var string = fs.readFileSync('./style.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){
+    var string = fs.readFileSync('./main.js', 'utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else if(path === '/pay'){ // 用户付款
+    var amount = fs.readFileSync('./db', 'utf8') // 100
+    var newAmount = amount - 1 // 类型从 string 转为 number
+      fs.writeFileSync('./db', newAmount) // 写入
+      response.setHeader('Content-Type', 'application/javascript')
+      response.statusCode = 200
+      response.write(`
+        alert("success")
+        amount.innerText = amount.innerText - 1 // 局部刷新：在服务器的js里改内容，通过write传到浏览器
+      `)
+      response.end()
   }else{
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write('找不到对应的路径，你需要自行修改 server.js')
     response.end()
   }
   
-
-
-
-
-
-
-
-
 
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请在空中转体720度然后用电饭煲打开 http://localhost:' + port)
 
 
